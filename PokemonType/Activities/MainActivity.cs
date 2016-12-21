b11ea0f0741e534129a096201a53b5f75213f8a6
John Fowler <jfowlerupcross@gmail.com>
2016-12-21 18:00:20 +0900

    Completely revamped UI

--- PokemonType/Activities/MainActivity.cs
+++ PokemonType/Activities/MainActivity.cs
@@ -1,14 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using System.IO;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using Android.Graphics;
-using Android.Views.Animations;
 using Android.Graphics.Drawables;
-using Android.Graphics.Drawables.Shapes;
-using Android.Views;
 using System;
 
 namespace PokemonType
@@ -21,6 +16,12 @@ namespace PokemonType
 		bool leftTurn = true;
 		GradientDrawable gradient;
 		List<TextView> textViews = new List<TextView>();
+		TextView left;
+		TextView right;
+
+		LinearLayout layout1;
+		LinearLayout layout2;
+		LinearLayout layout3;
 
 		protected override void OnResume()
 		{
@@ -34,10 +35,6 @@ namespace PokemonType
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.Main);
-
-			TextView left = FindViewById<TextView>(Resource.Id.topLeft);
-			TextView right = FindViewById<TextView>(Resource.Id.topRight);
-			Button goBtn = FindViewById<Button>(Resource.Id.go);
 
 			TextView textView1 = FindViewById<TextView>(Resource.Id.textView1);
 			TextView textView2 = FindViewById<TextView>(Resource.Id.textView2);
@@ -62,15 +59,14 @@ namespace PokemonType
 			{ textView1, textView2, textView3, textView4, textView5, textView6, textView7, textView8, textView9, textView10,
 				textView11, textView12, textView13, textView14, textView15, textView16, textView17, textView18};
 
-			string response;
-			StreamReader strm = new StreamReader(Assets.Open("AttackTypeChart.txt"));
-			response = strm.ReadToEnd();
-			allTypes.attackTypes = JsonConvert.DeserializeObject<List<Type>>(response);
+			layout1 = FindViewById<LinearLayout>(Resource.Id.layout1);
+			layout2 = FindViewById<LinearLayout>(Resource.Id.layout2);
+			layout3 = FindViewById<LinearLayout>(Resource.Id.layout3);
 
-			string response2;
-			StreamReader strm2 = new StreamReader(Assets.Open("DefenseTypeChart.txt"));
-			response2 = strm2.ReadToEnd();
-			allTypes.defenseTypes = JsonConvert.DeserializeObject<List<Type>>(response2);
+			left = FindViewById<TextView>(Resource.Id.leftTop);
+			right = FindViewById<TextView>(Resource.Id.rightTop);
+
+			GetTypeLists.GetEnglishLists(Assets);
 
 			var types = allTypes.defenseTypes;
 			for (int i = 0; i < types.Count;i++) 
@@ -82,89 +78,92 @@ namespace PokemonType
 				gradient = (GradientDrawable)textViews[i].Background;
 				gradient.SetColor(Color.ParseColor(Colors.TypeToColor[types[i].type]));
 
-				textViews[i].Click += (sender, e) => 
-				{
-					int num = (int)((TextView)sender).Tag;
-
-					GetTimeSpan(textViews[num], num);
-
-					gradient = (GradientDrawable)textViews[num].Background;
-
-					int delete = -1;
-					for (int j = 0; j < SendData.typeNum.Count; j++)
-					{
-						if (SendData.typeNum[j] == num)
-							delete = j;
-					}
-
-					if (delete != -1)
-					{
-						if (delete == 1)
-							removeTop(right, 1);
-						else if (delete == 0 && SendData.typeNum.Count == 2)
-							removeTop(left, 0);
-						
-						else if (delete == 0 && SendData.typeNum.Count == 1 && right.Text != "")
-							removeTop(right, 0);
-						else
-							removeTop(left, 0);
-					}
-					else
-					{
-						gradient.SetColor(Color.Aqua);
-
-						if (left.Text == "")
-						{
-							left.Text = types[num].type;
-							left.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[left.Text]));
-							//SendData.sendType.Add(types[num]);
-							SendData.typeNum.Add(num);
-						}
-						else if (right.Text == "")
-						{
-							right.Text = types[num].type;
-							right.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[right.Text]));
-							//SendData.sendType.Add(types[num]);
-							SendData.typeNum.Add(num);
-						}
-						else if (leftTurn)
-						{
-							left.Text = types[num].type;
-							left.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[left.Text]));
-							leftTurn = false;
-							gradient = (GradientDrawable)textViews[SendData.typeNum[0]].Background;
-							gradient.SetColor(Color.ParseColor(Colors.TypeToColor[types[SendData.typeNum[0]].type]));
-							SendData.typeNum[0] = num;
-							//SendData.sendType[0] = types[num];
-						}
-						else if (!leftTurn)
-						{
-							right.Text = types[num].type;
-							right.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[right.Text]));
-							leftTurn = true;
-							gradient = (GradientDrawable)textViews[SendData.typeNum[1]].Background;
-							gradient.SetColor(Color.ParseColor(Colors.TypeToColor[types[SendData.typeNum[1]].type]));
-							SendData.typeNum[1] = num;
-							//SendData.sendType[1] = types[num];
-						}
-					}
-				};
+				textViews[i].Click += Handle_Click;
 			}
-			goBtn.Click += (sender, e) =>
+			//goBtn.Click += (sender, e) =>
+			//{
+			//	if (SendData.sendType != null && SendData.typeNum.Count > 0)
+			//	{
+			//		SendData.kind = "Defense";
+			//		var sendTypes = allTypes.defenseTypes;
+			//		for (int i = 0; i < SendData.typeNum.Count; i++)
+			//		{
+			//			SendData.sendType.Add(sendTypes[SendData.typeNum[i]]);
+			//		}
+			//		StartActivity(typeof(test));
+			//	}
+			//	else
+			//		Toast.MakeText(this, "Nothing selected", ToastLength.Short).Show();
+			//};
+		}
+
+		void Handle_Click(object sender, EventArgs e)
+		{
+			var types = allTypes.defenseTypes;
+			int num = (int)((TextView)sender).Tag;
+
+			GetTimeSpan(textViews[num], num);
+
+			gradient = (GradientDrawable)textViews[num].Background;
+
+			int delete = -1;
+			for (int j = 0; j < SendData.typeNum.Count; j++)
 			{
-				if (SendData.sendType != null && SendData.typeNum.Count > 0)
-				{
-					SendData.kind = "Defense";
-					var sendTypes = allTypes.defenseTypes;
-					for (int i = 0; i < SendData.typeNum.Count; i++)
-					{
-						SendData.sendType.Add(sendTypes[SendData.typeNum[i]]);
-					}
-					StartActivity(typeof(TypeDetailActivity));
-				}
+				if (SendData.typeNum[j] == num)
+					delete = j;
+			}
+
+			if (delete != -1)
+			{
+				if (delete == 1)
+					removeTop(right, 1);
+				else if (delete == 0 && SendData.typeNum.Count == 2)
+					removeTop(left, 0);
+
+				else if (delete == 0 && SendData.typeNum.Count == 1 && right.Text != "")
+					removeTop(right, 0);
 				else
-					Toast.MakeText(this, "Nothing selected", ToastLength.Short).Show();
-			};
+					removeTop(left, 0);
+			}
+			else
+			{
+				gradient.SetColor(Color.Aqua);
+
+				if (left.Text == "")
+				{
+					left.Text = types[num].type;
+					left.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[left.Text]));
+					//SendData.sendType.Add(types[num]);
+					SendData.typeNum.Add(num);
+				}
+				else if (right.Text == "")
+				{
+					right.Text = types[num].type;
+					right.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[right.Text]));
+					//SendData.sendType.Add(types[num]);
+					SendData.typeNum.Add(num);
+				}
+				else if (leftTurn)
+				{
+					left.Text = types[num].type;
+					left.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[left.Text]));
+					leftTurn = false;
+					gradient = (GradientDrawable)textViews[SendData.typeNum[0]].Background;
+					gradient.SetColor(Color.ParseColor(Colors.TypeToColor[types[SendData.typeNum[0]].type]));
+					SendData.typeNum[0] = num;
+					//SendData.sendType[0] = types[num];
+				}
+				else if (!leftTurn)
+				{
+					right.Text = types[num].type;
+					right.SetBackgroundColor(Color.ParseColor(Colors.TypeToColor[right.Text]));
+					leftTurn = true;
+					gradient = (GradientDrawable)textViews[SendData.typeNum[1]].Background;
+					gradient.SetColor(Color.ParseColor(Colors.TypeToColor[types[SendData.typeNum[1]].type]));
+					SendData.typeNum[1] = num;
+					//SendData.sendType[1] = types[num];
+				}
+			}
 		}
 
 		public void removeTop(TextView top, int space)
@@ -175,12 +174,6 @@ namespace PokemonType
 
 			SendData.typeNum.RemoveAt(space);
 			//SendData.sendType.RemoveAt(space);
-		}
-		public string getColor(string type)
-		{
-			string color = "";
-
-			return color;
 		}
 
 		public bool GetTimeSpan(TextView TF, int num)
---
+++ PokemonType/GetTypeLists.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PokemonType
+{
+	public class GetTypeLists
+	{
+		public static void GetEnglishLists(Android.Content.Res.AssetManager Assets)
+		{
+			string response;
+			StreamReader strm = new StreamReader(Assets.Open("AttackTypeChart.txt"));
+			response = strm.ReadToEnd();
+			allTypes.attackTypes = JsonConvert.DeserializeObject<List<Type>>(response);
+
+			string response2;
+			StreamReader strm2 = new StreamReader(Assets.Open("DefenseTypeChart.txt"));
+			response2 = strm2.ReadToEnd();
+			allTypes.defenseTypes = JsonConvert.DeserializeObject<List<Type>>(response2);
+		}
+
+		public static void GetJapaneseLists(Android.Content.Res.AssetManager Assets)
+		{
+			string response;
+			StreamReader strm = new StreamReader(Assets.Open("JapaneseAttackType.txt"));
+			response = strm.ReadToEnd();
+			allTypes.attackTypes = JsonConvert.DeserializeObject<List<Type>>(response);
+
+			string response2;
+			StreamReader strm2 = new StreamReader(Assets.Open("JapaneseDefenseType.txt"));
+			response2 = strm2.ReadToEnd();
+			allTypes.defenseTypes = JsonConvert.DeserializeObject<List<Type>>(response2);
+		}
+	}
+}
